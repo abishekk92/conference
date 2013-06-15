e1a6468e62da4bc756460cb15b4feb9279432da5
Abishek Bhat <bhat.abishek@gmail.com>
2013-06-15 18:51:38 +0530

    Changes to reflect with changes in the model and the view

--- app.py
+++ app.py
@@ -1,9 +1,65 @@
 from flask import Flask,request,Response,render_template,redirect,url_for,make_response
 import plivo
+
+from models import get_all_conference,create_conference,get_mobile,get_record
+
+from helper import rent_number,generate_pin
+
+from config_parser import get_config
+
+configs=get_config()
+
 app=Flask(__name__)
+
 app.debug=True
-CONFERENCE_PIN='1235'
+
+CALLER_ID="19095002259"
+
 BASE_URL='http://conference-call.herokuapp.com'
+
+plivo_api=plivo.RestAPI(configs["AUTH_ID"],configs["AUTH_TOKEN"])
+
+
+
+def notify(message,mobile):
+	response=plivo_api.send_message({'src':CALLER_ID,
+				'dst':mobile,
+				'text':message,
+				'type':'sms'
+				})
+	return response
+
+@app.route('/')
+
+def index():
+	return render_template('index.html',
+			conferences=get_all_conference()
+				)
+
+
+@app.route('/create')
+
+def create():
+	subject=request.args.get('subject','')
+	phone=request.args.get('phone','')
+	record_value=request.args.get('record','')
+	record=False
+	members=[request.args.get('member0',''),request.args.get('member1','0')]
+	if record_value=="on":
+		record=True
+	conference_number=rent_number(plivo_api)
+	conference_pin=generate_pin(conference_number)
+	create_conference(subject,phone,members,record,conference_number,conference_pin)
+	message="Hi, you can join the conference by dialing %s and the pin number being %s" %(conference_number,conference_pin)
+	for member in members:
+		response=notify(message,member)
+		print response
+
+	return "Conference created and members notified"
+
+
+
+
 @app.route('/conference')
 
 def conference():
@@ -25,14 +81,19 @@ def conference():
 
 def verify_pin():
 	pin=request.args.get('Digits','')
+	conference_number=request.args.get('To','')
 	response=plivo.Response()
-	if pin != CONFERENCE_PIN:
+	record_value=False
+	if pin!=generate_pin(conference_number):
+
 		response.addSpeak("Such a conference does not exist, please try again with a valid conference pin")
 		response.addHangup()
 	else:
+		if get_record(conference_number):
+			record_value=True
 		response.addConference(body='plivo',
 				       action= BASE_URL+url_for('submit_recording'),
-				       record=True)
+				       record=record_value)
 	xml_response=make_response(response.to_xml())
 	xml_response.headers["Content-type"]="text/xml"
 	return xml_response 
@@ -40,14 +101,13 @@ def verify_pin():
 
 def submit_recording():
 	record_url=request.args.get('RecordUrl','')
+	conference_number=request.args.get('To','')
 	print record_url
-	return record_url
-
-
-
-
-
-
+	message="The recording of your conference call is available at %s. Thanks for using Plivo" %(record_url)
+	mobile=get_mobile(conference_number)
+	response=notify(message,mobile)
+	print response
+	return "Success"
 
 
 if __name__=='__main__':
